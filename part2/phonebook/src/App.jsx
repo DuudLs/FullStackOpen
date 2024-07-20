c6ed21b77b43520edf818eada9162055fcfce677
DuudLs <kj.joosing@gmail.com>
2024-07-20 16:12:47 +0300

    20.07.24

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter.jsx'
 import PersonForm from './components/PersonForm.jsx'
 import Persons from './components/Persons.jsx'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredName, setFilteredName] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
-        filter <Filter filteredName={filteredName} setFilteredName={setFilteredName} />
+        Filter <Filter filteredName={filteredName} setFilteredName={setFilteredName} />
       </div>
-      <h2>add a new</h2>
+      <h2>Add a new</h2>
       <PersonForm persons={persons} setPersons={setPersons}	newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
       <h2>Contacts</h2>
       <Persons persons={persons} setPersons={setPersons} filteredName={filteredName} setFilteredName={setFilteredName} />
